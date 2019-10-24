25343e0f2b63f63ed34b6fa037d6bc85aeeae16d
Abdulrahman Abdulkarim <olanre12waju@gmail.com>
2019-10-24 18:36:22 +0100

    Initial Commit

---
+++ Bank AI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bank AI
+//
+//  Created by Abdulrahman Abdulkarim on 10/24/19.
+//  Copyright © 2019 Abdulrahman Abdulkarim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
